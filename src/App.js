9af29bbf1055252becbba16630d5c4c16601087a
Zihe Zhang <51214897+zihezhang@users.noreply.github.com>
2020-07-19 17:23:54 -0400

    Orders and OpenFoods hooks

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import { Banner } from './Banner/Banner';
 import { Menu } from './Menu/Menu';
 import { FoodDialog } from './FoodDialog/FoodDialog'
 import { Order } from './Order/Order';
+import { useOpenFood } from './Hooks/useOpenFood';
+import { useOrders } from './Hooks/useOrders';
 
 function App() {
-  const [openFood, setOpenFood] = useState();
+  const openFood = useOpenFood();
+  const orders = useOrders();
   return (
     <React.Fragment>
       <GlobalStyle/>
-      <FoodDialog openFood = {openFood} setOpenFood={setOpenFood}/>
+      <FoodDialog {...openFood} {...orders}/>
       <Navbar/>
-      <Order/>
+      <Order {...orders}/>
       <Banner/>
-      <Menu setOpenFood={setOpenFood}/>
+      <Menu {...openFood}/>
     </React.Fragment>
   );
 }
